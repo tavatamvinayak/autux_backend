b7bc986f196cb9f76e93bf5c2f96fefe79f90b4f
vinayak tavatam <107667781+tavatamvinayak@users.noreply.github.com>
2025-10-31 14:57:55 +0530

    Update Item.js

--- models/Item.js
+++ models/Item.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  highlight: { type: String, required: true },
-  subtitle: { type: String, required: true }
+  title: { type: String },
+  highlight: { type: String},
+  subtitle: { type: String },
+  url: { type: String }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Item', itemSchema);
